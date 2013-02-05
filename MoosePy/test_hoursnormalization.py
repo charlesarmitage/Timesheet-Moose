6f0e4aae19200cccdba04e940ea33e87e32b4aad
Charles Armitage <code@cjrarmitage.co.uk>
2013-02-05 23:01:38 +0000

    Add rounding for hours
    Round up (for end time) for hours that are within 3 minutes of the next
    quarter
    Round down (for start time) for hours within 3 minutes of previous
    quarter

--- MoosePy/test_hoursnormalization.py
+++ MoosePy/test_hoursnormalization.py
@@ -14,9 +14,33 @@ def previousquarter(workingtime):
     quarter = quarter * 15
     return workingtime.replace(minute=quarter)
 
+def nextquarter(workingtime):
+    quarter = (workingtime.minute / 15) + 1
+    quarter = (quarter * 15) % 60
+    rolloverhour = workingtime.hour
+    if quarter == 0:
+        rolloverhour = workingtime.hour + 1
+    return workingtime.replace(hour=rolloverhour, minute=quarter)
+
+def is_close_to_nextquarter(nextquarter, currenttime):
+    return nextquarter.minute - currenttime.minute <= 3
+
+def is_close_to_previousquarter(previousquarter, currenttime):
+    return currenttime.minute - previousquarter.minute <= 3
+
 def normalizehours(hours):
-    hours.start = previousquarter(hours.start)
-    hours.end = previousquarter(hours.end)
+    nextq = nextquarter(hours.end)
+    previousq = previousquarter(hours.start)
+
+    if is_close_to_nextquarter(nextq, hours.end):
+        hours.end = nextq
+    else:
+        hours.end = previousquarter(hours.end)
+
+    if is_close_to_previousquarter(previousq, hours.start):
+        hours.start = previousquarter(hours.start)
+    else:
+        hours.start = nextquarter(hours.start)
 
     return hours
 
@@ -52,3 +76,31 @@ class TestNormalization(unittest.TestCase):
         hours = normalizehours(self.hours)
 
         assert hours.end == datetime.time(16, 30, 0)
+
+    def test_workingday_ending_at_17_14_ends_at_5_15pm(self):
+        self.hours.end = datetime.time(17, 14)
+
+        hours = normalizehours(self.hours)
+
+        assert hours.end == datetime.time(17, 15)
+
+    def test_workingday_ending_at_16_58_ends_at_5pm(self):
+        self.hours.end = datetime.time(16, 58)
+
+        hours = normalizehours(self.hours)
+
+        assert hours.end == datetime.time(17, 0)
+
+    def test_workingday_starting_at_9_04_starts_at_9_15am(self):
+        self.hours.start = datetime.time(9, 04)
+
+        hours = normalizehours(self.hours)
+
+        assert hours.start == datetime.time(9, 15)
+
+    def test_workingday_starting_at_8_50_starts_at_9am(self):
+        self.hours.start = datetime.time(8, 50)
+
+        hours = normalizehours(self.hours)
+
+        assert hours.start == datetime.time(9, 00)
