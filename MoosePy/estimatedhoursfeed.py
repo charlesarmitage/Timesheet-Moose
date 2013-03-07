a7e88a941bc5058eea4e6f68497cf9f50f7f7491
Charles Armitage <code@cjrarmitage.co.uk>
2013-03-07 12:48:05 +0000

    Add very simple web front end for viewing hours
    Use Nancy framework to read and view estimated hours.
    Display very simple page and use IronPython scripts to get data.

--- MoosePy/estimatedhoursfeed.py
+++ MoosePy/estimatedhoursfeed.py
@@ -10,6 +10,7 @@ from workinghours import hours_input
 from workinghours import hours_estimator
 
 logfile = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + r"\Timesheet.log"
-raw_hours = hours_input.readfromlog(logfile)
-hours = hours_feed.calculate_estimated_hours(datetime.datetime.today(), raw_hours)
+hours = hours_input.readfromlog(logfile)
+#hours = reversed(hours)
+hours = hours_feed.calculate_estimated_hours(datetime.datetime.today(), hours)
 hours = hours_estimator.get_estimated_hours(hours)
---
+++ TimesheetWeb/TimesheetWeb/TimesheetModule.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Web;
+using IronPython.Hosting;
+using Nancy;
+using System.Web;
+
+namespace TimesheetWeb
+{
+    public class ViewOutput
+    {
+        public string month;
+        public dynamic hours;
+    }
+
+    public class TimesheetModule : NancyModule
+    {
+        private readonly IRootPathProvider pathProvider;
+
+        public TimesheetModule(IRootPathProvider pathProvider)
+        {
+            this.pathProvider = pathProvider;
+
+            Get["/"] = parameters =>
+                           {
+                               var workingHours = GenerateWorkingHours();
+                               var m = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(DateTime.Now.Month);
+                               var output = new ViewOutput{hours = workingHours, month = m};
+                               return View["TimesheetIndex.cshtml", output];
+                           };
+        }
+
+        private dynamic GenerateWorkingHours()
+        {
+            var ipy = Python.CreateRuntime();
+            string path = Path.Combine(pathProvider.GetRootPath(), @"..\..\MoosePy\estimatedhoursfeed.py");
+            dynamic estimatedHours = ipy.UseFile(path);
+            return estimatedHours.hours;
+        }
+    }
+}
