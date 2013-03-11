5e800b181ec247463e8b2def2a48d73b6d18f0ac
Charles Armitage <code@cjrarmitage.co.uk>
2013-03-11 13:19:44 +0000

    Group hours by week
    Group hours by week for display. How to seperate this view concern from
    the model to be worked out later.

--- MoosePy/workinghours/hours_aggregation.py
+++ MoosePy/workinghours/hours_aggregation.py
@@ -7,5 +7,11 @@ def group_hours_by_day(hours):
         days.append(list(day))
     return days
 
+def group_hours_by_week(hours):
+	days = []
+	for date, day in groupby(hours, key=lambda d: d.date.isocalendar()[1]):
+		days.append(list(day))
+	return days
+
 def normalize_start_and_end_times(rawHours):
 	return hours_normalization.normalizehours(rawHours)
--- MoosePy/workinghours/hours_feed.py
+++ MoosePy/workinghours/hours_feed.py
@@ -4,8 +4,12 @@ import hours_filtering
 import hours_normalization
 
 def calculate_estimated_hours(startday, raw_hours):
+	normalized_hours = calculate_estimated_ungrouped_hours(startday, raw_hours)
+	hours_grouped_by_day = hours_aggregation.group_hours_by_day(normalized_hours)
+	return hours_grouped_by_day
+
+def calculate_estimated_ungrouped_hours(startday, raw_hours):
     hours = hours_filtering.filter_by__current_worksheet_month(startday, raw_hours)
     hours = hours_filtering.remove_weekends(hours)
     normalized_hours = hours_normalization.normalize_hours_list(hours)
-    hours_grouped_by_day = hours_aggregation.group_hours_by_day(normalized_hours)
-    return hours_grouped_by_day
+    return normalized_hours	
