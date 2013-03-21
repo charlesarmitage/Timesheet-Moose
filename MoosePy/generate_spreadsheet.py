ccaa4b25312dd35ee620a609d721cde8c000007d
Charles Armitage <code@cjrarmitage.co.uk>
2013-03-21 22:52:46 +0000

    Pass spreadsheet file as a parameter in script
    Pass the xls file as a parameter to the spreadsheet writing functions.

--- MoosePy/generate_spreadsheet.py
+++ MoosePy/generate_spreadsheet.py
@@ -1,25 +1,30 @@
 import sys
+import shutil
 import workbooknav
 import reportoutput
 import estimatedhoursinweeks
 
-def write_hours_to_report(hours):
-    xls_file = r"C:\git\Timesheet-Moose\MooseXLSReports\Testtimesheet.xlsx"
+def write_hours_to_report(xls_file, hours):
     writer = reportoutput.build_ipy_writer(xls_file)
     for estimated_hours in hours:
         writer.write(estimated_hours)
 
-def write_hours(weeks):
+    writer.close()
+
+def write_hours(xls_file, weeks):
 	hours = []
 	for week in weeks:
 		hours.extend(week)
 
 	print hours
-	write_hours_to_report(hours)
+	write_hours_to_report(xls_file, hours)
 
 if __name__ == '__main__':
 	if 'weeks' not in globals():
 		logfile = sys.argv[1]
 		weeks = estimatedhoursinweeks.generate_estimated_hours(logfile)
 	
-	write_hours(weeks)
+	xls_file = r"C:\git\Timesheet-Moose\MooseXLSReports\Testtimesheet.xlsx"
+	shutil.copyfile(xls_file, xls_file + ".tmp")
+
+	write_hours(xls_file + ".tmp", weeks)
