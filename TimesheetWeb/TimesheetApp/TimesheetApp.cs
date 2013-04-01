ee14fd9a829e8b67d86e9d469504d9cf174480be
Charles Armitage <code@cjrarmitage.co.uk>
2013-04-01 10:47:31 +0100

    Add Timesheet module to standalone app

--- TimesheetWeb/TimesheetApp/TimesheetApp.cs
+++ TimesheetWeb/TimesheetApp/TimesheetApp.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Nancy;
+using TimesheetWeb;
 
 namespace TimesheetWebApp
 {
@@ -17,20 +19,12 @@ namespace TimesheetWebApp
             InitializeComponent();
 
             var nancyHost = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:41978"));
-
+            TimesheetModule module = new TimesheetModule();
             nancyHost.Start();
 
-
+            // TODO: Rename to TimeClerk for app name
 
             this.FormClosed += (sender, args) => nancyHost.Stop();
-        }
-    }
-
-    public class TestModule : NancyModule
-    {
-        public TestModule(IRootPathProvider pathProvider)
-        {
-            Get["/"] = parameters => "Hello";
         }
     }
 }
