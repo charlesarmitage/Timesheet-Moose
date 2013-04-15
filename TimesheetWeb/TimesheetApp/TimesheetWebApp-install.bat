39188d7fa6749680cf359fe9e834ab8ede99aad7
Charles Armitage <code@cjrarmitage.co.uk>
2013-04-15 15:49:01 +0100

    Use current user as username for install .bat

--- TimesheetWeb/TimesheetApp/TimesheetWebApp-install.bat
+++ TimesheetWeb/TimesheetApp/TimesheetWebApp-install.bat
@@ -1,4 +1,5 @@
 echo Timesheet Web App installation.
 echo Allows non-admistrators to listen for http on port 41978
-echo Run as administrator and change user.
-netsh http add urlacl url=http://localhost:41978/ user=cjra
+echo Run as administrator.
+echo %USERNAME%
+netsh http add urlacl url=http://localhost:41978/ user=%USERNAME%
